63fe367fa9d6ad93f0cc8f5ff1f43605a527dafb
malygin-as <alex-malygin2007@ya.ru>
2022-07-24 19:52:34 +0700

    4 comit

--- crm/views.py
+++ crm/views.py
@@ -4,4 +4,7 @@ from .models import Order
 # Create your views here.
 def first_page(request):
     object_list = Order.objects.all()
-    return render(request, './index.html', {'object_list' : object_list})
+    return render(request, './index.html', {'object_list': object_list})
+
+def thanks_page(request):
+    return render(request, './thanks_page.html')
